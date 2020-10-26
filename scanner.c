c5279fcfdd80424006f2e28b61fbe8760563509f
Ja <lukashais@disroot.org>
2020-10-26 18:33:56 +0100

    revert Horslandis fuckup

---
+++ scanner.c
@@ -0,0 +1,602 @@
+/**
+ * Project: Implementace překladače imperativního jazyka IFJ20
+ *
+ * @file scanner.c
+ * @brief Definition of scanner's functions
+ *
+ * @authors
+ *      Hais Lukas (xhaisl00)
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+#include "scanner.h"
+
+int setSourceFile (FILE *file)
+{
+  if (file != NULL)
+  {
+    sourceFile = file;
+    return 0;
+  }
+  return 1;
+}
+
+void unsetSourceFile ()
+{
+  sourceFile = NULL;
+}
+
+int isReservedKeyword (string *str)
+{
+  // maybe this is not the best approach (discussion necessary)
+  if (strCmpConstStr(str, "package") == 0)
+    return 1;
+  else if (strCmpConstStr(str, "import") == 0)
+    return 2;
+  else if (strCmpConstStr(str, "func") == 0)
+    return 3;
+  else if (strCmpConstStr(str, "return") == 0)
+    return 4;
+  else if (strCmpConstStr(str, "print") == 0)
+    return 5;
+  else if (strCmpConstStr(str, "var") == 0)
+    return 6;
+  else if (strCmpConstStr(str, "const") == 0)
+    return 7;
+  else if (strCmpConstStr(str, "if") == 0)
+    return 8;
+  else if (strCmpConstStr(str, "else") == 0)
+    return 9;
+  else if (strCmpConstStr(str, "switch") == 0)
+    return 10;
+  else if (strCmpConstStr(str, "case") == 0)
+    return 11;
+  else if (strCmpConstStr(str, "default") == 0)
+    return 12;
+  else if (strCmpConstStr(str, "for") == 0)
+    return 13;
+  else if (strCmpConstStr(str, "range") == 0)
+    return 14;
+  else if (strCmpConstStr(str, "string") == 0)
+    return 15;
+  else if (strCmpConstStr(str, "int") == 0)
+    return 16;
+  else if (strCmpConstStr(str, "float64") == 0)
+    return 17;
+  else return 0;
+}
+
+void setTokenKeyword (Token *token, int keywordType)
+{
+  token->type = TYPE_KEYWORD;
+  switch (keywordType)
+  {
+    case 1:
+      token->attribute.keyword = KEYWORD_PACKAGE;
+      break;
+    case 2:
+      token->attribute.keyword = KEYWORD_IMPORT;
+      break;
+    case 3:
+      token->attribute.keyword = KEYWORD_FUNC;
+      break;
+    case 4:
+      token->attribute.keyword = KEYWORD_RETURN;
+      break;
+    case 5:
+      token->attribute.keyword = KEYWORD_PRINT;
+      break;
+    case 6:
+      token->attribute.keyword = KEYWORD_VAR;
+      break;
+    case 7:
+      token->attribute.keyword = KEYWORD_CONST;
+      break;
+    case 8:
+      token->attribute.keyword = KEYWORD_IF;
+      break;
+    case 9:
+      token->attribute.keyword = KEYWORD_ELSE;
+      break;
+    case 10:
+      token->attribute.keyword = KEYWORD_SWITCH;
+      break;
+    case 11:
+      token->attribute.keyword = KEYWORD_CASE;
+      break;
+    case 12:
+      token->attribute.keyword = KEYWORD_DEFAULT;
+      break;
+    case 13:
+      token->attribute.keyword = KEYWORD_FOR;
+      break;
+    case 14:
+      token->attribute.keyword = KEYWORD_RANGE;
+      break;
+    case 15:
+      token->attribute.keyword = KEYWORD_STRING;
+      break;
+    case 16:
+      token->attribute.keyword = KEYWORD_INT;
+      break;
+    case 17:
+      token->attribute.keyword = KEYWORD_FLOAT64;
+      break;
+  }
+}
+
+int getToken (Token *token)
+{
+  if (token == NULL)
+  {
+    return 2;
+  }
+
+  // char read from @var sourceFile
+  char c;
+  // initialization of state
+  State state = STATE_START;
+
+  while (1)
+  {
+    // read another character
+    c = getc(sourceFile);
+
+    // enter deterministic automaton
+    switch (state)
+    {
+      case STATE_START:
+        /** DONE **/
+        if (c == EOF)
+        {
+          printf("[EOF]\n");
+          token->type = TYPE_EOF;
+          return 0;
+        }
+        else if (c == '/')
+        {
+          state = STATE_DIVIDE;
+        }
+        else if (c == '+')
+        {
+          state = STATE_PLUS;
+        }
+        else if (c == '-')
+        {
+          state = STATE_MINUS;
+        }
+        else if (c == '*')
+        {
+          state = STATE_MULTIPLY;
+        }
+        else if (c == '_' || isalpha(c))
+        {
+          // decide if identifier only by leading underscore, ID/KEY otherwise
+          if (c == '_')
+          {
+            state = STATE_IDENTIFIER;
+          }
+          else
+          {
+            state = STATE_IDENTIFIER_OR_KEYWORD;
+          }
+
+          // initialize new string and check if it was successful
+          if (strInit(&token->attribute.string))
+          {
+            token->type = TYPE_EMPTY;
+            printf("Internal error when allocating string, exiting...\n");
+            return 3;
+          }
+
+          // set new character to string
+          strAddChar(&token->attribute.string, c);
+        }
+        else if (c == '(')
+        {
+          printf("[(] ");
+          token->type = TYPE_LEFT_BRACKET;
+          return 0;
+        }
+        else if (c == ')')
+        {
+          printf("[)] ");
+          token->type = TYPE_RIGHT_BRACKET;
+          return 0;
+        }
+        else if (c == '{')
+        {
+          token->type = TYPE_LEFT_CURLY_BRACKET;
+          printf("[{]");
+          return 0;
+        }
+        else if (c == '}')
+        {
+          token->type = TYPE_RIGHT_CURLY_BRACKET;
+          printf("[}] ");
+          return 0;
+        }
+        else if (c == '"')
+        {
+          // initialize new string and check if it was successful
+          if (strInit(&token->attribute.string))
+          {
+            token->type = TYPE_EMPTY;
+            printf("Internal error when allocating string, exiting...\n");
+            return 3;
+          }
+
+          state = STATE_STRING;
+        }
+        else if (c == ':')
+        {
+          state = STATE_DECLARATIVE_ASSIGN;
+        }
+        else if (c == ',')
+        {
+          printf("[,] ");
+          token->type = TYPE_COMMA;
+          return 0;
+        }
+        else if (c == '=')
+        {
+          printf("[=] ");
+          token->type = TYPE_DECLARATIVE_ASSIGN;
+          return 0;
+        }
+        else if (c == '<')
+        {
+          printf("[<] ");
+          token->type = TYPE_LESSER;
+          return 0;
+        }
+        else if (c == '>')
+        {
+          printf("[>] ");
+          token->type = TYPE_GREATER;
+          return 0;
+        }
+        else if (c == ';')
+        {
+          printf("[;] ");
+          token->type = TYPE_SEMICOLON;
+          return 0;
+        }
+        else if (c == '!')
+        {
+          state = STATE_NOT_EQUALS;
+        }
+        /** END DONE **/
+        else if (isdigit(c))
+        {
+          state = STATE_DIGIT;
+          token->attribute.integer = (int) c - 48;
+        }
+        // done
+        else if (isspace(c))
+        {
+          if (c == '\n')
+            printf("\n");
+
+          continue;
+        }
+        else
+        {
+          printf("\n");
+          return 1;
+        }
+        break;
+
+      case STATE_NOT_EQUALS:
+        if (c == '=')
+        {
+          token->type = TYPE_NOT_EQUALS;
+          return 0;
+        }
+        else return 1;
+        break;
+      case STATE_DECLARATIVE_ASSIGN:
+        if (c == '=')
+        {
+          printf("[:=] ");
+          token->type = TYPE_DECLARATIVE_ASSIGN;
+          return 0;
+        }
+        else return 1;
+
+      case STATE_STRING:
+        if (c == '"')
+        {
+          printf("STRING[%s] ", strGetStr(&token->attribute.string));
+          token->type = TYPE_STRING;
+          return 0;
+        }
+        else if (c == 92)
+        {
+          if (strAddChar(&token->attribute.string, c))
+          {
+            strFree(&token->attribute.string);
+            printf("Unable to realloc token's attribute string.\n");
+            return 3;
+          }
+          state = STATE_STRING_SKIP;
+        }
+        else if (c == EOF)
+        {
+          strFree(&token->attribute.string);
+          printf("String didn't end with trailing \".\n");
+          return 1;
+        }
+        else
+        {
+          // check whether adding of char was successful
+          if (!strAddChar(&token->attribute.string, c)) continue;
+          else
+          {
+            strFree(&token->attribute.string);
+            printf("Unable to realloc token's attribute string.\n");
+            return 3;
+          }
+        }
+        break;
+      case STATE_STRING_SKIP:
+        if (c == EOF)
+        {
+          strFree(&token->attribute.string);
+          printf("String didn't end with trailing \".\n");
+          return 1;
+        }
+        else
+        {
+          // check whether adding of char was successful
+          if (strAddChar(&token->attribute.string, c))
+          {
+            strFree(&token->attribute.string);
+            printf("Unable to realloc token's attribute string.\n");
+            return 3;
+          }
+
+          state = STATE_STRING;
+        }
+        break;
+      case STATE_DIGIT:
+        if (isdigit(c))
+        {
+          token->attribute.integer *= 10;
+          token->attribute.integer += (int) c - 48;
+        }
+        else if (isspace(c) || c == EOF || c == '\n' || ispunct(c))
+        {
+          if (ispunct(c))
+            ungetc(c, sourceFile);
+
+          printf("INT[%lld] ", (long long) token->attribute.integer);
+          if (c == '\n') printf("\n");
+
+          token->type = TYPE_INT;
+          return 0;
+        }
+        else
+        {
+          return 1;
+        }
+        break;
+      case STATE_DIGIT_NEGATIVE:
+        if (isdigit(c))
+        {
+          token->attribute.integer *= 10;
+          token->attribute.integer -= (int) c - 48;
+        }
+        else if (isspace(c) || c == EOF || c == '\n' || ispunct(c))
+        {
+          if (ispunct(c))
+            ungetc(c, sourceFile);
+
+          printf("INT[%lld] ", (long long) token->attribute.integer);
+          if (c == '\n') printf("\n");
+
+          token->type = TYPE_INT;
+          return 0;
+        }
+        else
+        {
+          return 1;
+        }
+        break;
+
+      /******   OPERATORS SECTION   ******/
+      /** STATE PLUS + MINUS NEEDS TO SAVE NUMBER TO CORRESPONDING VARIABLE **/
+      case STATE_PLUS:
+        if (isspace(c) || c == EOF || c == '\n')
+        {
+          printf("[+] ");
+          if (c == '\n')
+            printf("\n");
+
+          token->type = TYPE_PLUS;
+          return 0;
+        }
+        else if (isdigit(c))
+        {
+          state = STATE_DIGIT;
+          token->attribute.integer = (int) c - 48;
+        }
+        else
+        {
+          return 1;
+        }
+        break;
+      case STATE_MINUS:
+        if (isspace(c) || c == EOF || c == '\n')
+        {
+          printf("[-] ");
+          if (c == '\n')
+            printf("\n");
+
+          token->type = TYPE_MINUS;
+          return 0;
+        }
+        else if (isdigit(c))
+        {
+          state = STATE_DIGIT_NEGATIVE;
+          token->attribute.integer = (int) (c - 48) * (-1);
+        }
+        else
+        {
+          return 1;
+        }
+        break;
+      case STATE_MULTIPLY:
+        if (isspace(c) || c == EOF || c == '\n')
+        {
+          printf("[*] ");
+          if (c == '\n')
+            printf("\n");
+
+          token->type = TYPE_MULTIPLY;
+          return 0;
+        }
+        else
+        {
+          return 1;
+        }
+      case STATE_DIVIDE:
+        if (c == '/')
+        {
+          state = STATE_LINE_COMMENT;
+        }
+        else if (c == '*')
+        {
+          state = STATE_MULTI_LINE_COMMENT_START;
+        }
+        else if (isspace(c) || c == EOF || c == '\n')
+        {
+          printf("[/] ");
+          if (c == '\n')
+            printf("\n");
+
+          token->type = TYPE_DIVIDE;
+          return 0;
+        }
+        else
+        {
+          return 1;
+        }
+        break;
+      /****** END OPERATORS SECTION ******/
+
+      /******   ID/KEY SECTION   ******/
+      case STATE_IDENTIFIER:
+        if (isalpha(c) || isdigit(c))
+        {
+          // check whether adding of char was successful
+          if (!strAddChar(&token->attribute.string, c)) continue;
+          else
+          {
+            strFree(&token->attribute.string);
+            printf("Unable to realloc token's attribute string.\n");
+            return 3;
+          }
+        }
+        else if (isspace(c) || c == '\n' || c == EOF || ispunct(c))
+        {
+          if (ispunct(c))
+          {
+            ungetc(c, sourceFile);
+          }
+
+          printf("ID[%s] ", strGetStr(&token->attribute.string));
+          if (c == '\n')
+            printf("\n");
+
+          token->type = TYPE_IDENTIFIER;
+          return 0;
+        }
+        else
+        {
+          strClear(&token->attribute.string);
+          return 1;
+        }
+      case STATE_IDENTIFIER_OR_KEYWORD:
+        if (isalpha(c) || isdigit(c))
+        {
+          // check whether adding of char was successful
+          if (!strAddChar(&token->attribute.string, c)) continue;
+          else
+          {
+            strFree(&token->attribute.string);
+            printf("Unable to realloc token's attribute string.\n");
+            return 3;
+          }
+        }
+        else if (isspace(c) || c == '\n' || c == EOF || ispunct(c))
+        {
+          if (ispunct(c))
+          {
+            ungetc(c, sourceFile);
+          }
+
+          /** CHECK IF KEYWORD OR IDENTIFIER **/
+          int code = isReservedKeyword(&token->attribute.string);
+          if (code != 0)
+          {
+            printf("KEY[%s] ", strGetStr(&token->attribute.string));
+            setTokenKeyword(token, code);
+          }
+          else
+          {
+            printf("ID[%s] ", strGetStr(&token->attribute.string));
+            token->type = TYPE_IDENTIFIER;
+          }
+
+          if (c == '\n')
+            printf("\n");
+
+          return 0;
+        }
+        else
+        {
+          strClear(&token->attribute.string);
+          return 1;
+        }
+      /****** END ID/KEY SECTION ******/
+
+      /******   COMMENTS SECTION   ******/
+      case STATE_LINE_COMMENT:
+        if (c == '\n' || c == EOF)
+        {
+          printf("[Line Comment]\n");
+
+          token->type = TYPE_EMPTY;
+          return 0;
+        }
+        break;
+      case STATE_MULTI_LINE_COMMENT_START:
+        if (c == '*' || c == EOF)
+        {
+          state = STATE_MULTI_LINE_COMMENT_END;
+        }
+        break;
+      case STATE_MULTI_LINE_COMMENT_END:
+        if (c == '/' || c == EOF)
+        {
+          printf("[Multi line comment]\n");
+
+          token->type = TYPE_EMPTY;
+          return 0;
+        }
+        else
+        {
+          state = STATE_MULTI_LINE_COMMENT_START;
+        }
+        break;
+      /****** END COMMENTS SECTION ******/
+
+      // PART OF SWITCH, DON'T DELETE
+      default:
+        printf("Uncaught state!");
+        return 1;
+    }
+  }
+}
---
+++ scanner.h
@@ -0,0 +1,218 @@
+/**
+ * Project: Implementace překladače imperativního jazyka IFJ20
+ *
+ * @name scanner.h
+ * @brief Scanner header file containing various definitions for scanner to use.
+ *
+ * @authors
+ *      Hais Lukas (xhaisl00)
+ */
+
+#ifndef IFJ_PROJ_SCANNER_H
+#define IFJ_PROJ_SCANNER_H
+
+// incldude for int_64t support
+#include <stdint.h>
+// include files for working with dynamic string
+#include "str.c"
+
+/**
+ * @brief Source file we are reading from.
+ */
+FILE *sourceFile = NULL;
+
+typedef enum
+{
+  STATE_START,
+
+  STATE_PLUS, // could be plus operator or positive number
+  STATE_MINUS, // could be minus operator or negative number
+  STATE_MULTIPLY,
+  STATE_DIVIDE, // could result in [multi] line comment or division operator
+
+  STATE_LINE_COMMENT,
+  STATE_MULTI_LINE_COMMENT_START,
+  STATE_MULTI_LINE_COMMENT_END,
+
+  STATE_IDENTIFIER, // if it starts with '_'
+  STATE_IDENTIFIER_OR_KEYWORD,
+
+  STATE_DIGIT, // step before INT / FLOAT
+  STATE_DIGIT_NEGATIVE,
+  STATE_INT,
+  STATE_FLOAT,
+
+  STATE_STRING,
+  STATE_STRING_SKIP, // used when '\' is read in string
+
+  STATE_ASSIGN,
+  STATE_DECLARATIVE_ASSIGN,
+  STATE_NOT_EQUALS,
+
+  STATE_BOOLEAN, // BOOLEAN EXTENSION
+} State;
+
+/**
+ * @enum Predefined keywords for @union TokenAttribute.
+ */
+typedef enum
+{
+  // PACKAGES
+  KEYWORD_PACKAGE, // 1
+  KEYWORD_IMPORT, // 2
+
+  // FUNCTIONS AND RETURN
+  KEYWORD_FUNC, // 3
+  KEYWORD_RETURN, // 4
+  KEYWORD_PRINT, // 5
+
+  // VARIABLES
+  KEYWORD_VAR, // 6
+  KEYWORD_CONST, // 7
+
+  // FLOW CONTROL
+  KEYWORD_IF, // 8
+  KEYWORD_ELSE, // 9
+  KEYWORD_SWITCH, // 10
+  KEYWORD_CASE, // 11
+  KEYWORD_DEFAULT, // 12
+  KEYWORD_FOR, // 13
+  KEYWORD_RANGE, // 14 (used in for-range loop)
+
+  // DATA TYPES
+  KEYWORD_STRING, // 15
+  KEYWORD_INT, // 16
+  KEYWORD_FLOAT64, // 17
+} Keyword;
+
+/**
+ * @enum Predefined @struct Token types.
+ */
+typedef enum
+{
+  // BASIC
+  TYPE_EOF,
+  TYPE_EOL,
+  TYPE_EMPTY, // used when scanner encounters comment
+  TYPE_IDENTIFIER,
+  TYPE_KEYWORD,
+
+  // DATA TYPES
+  TYPE_INT, // 5
+  TYPE_STRING,
+  TYPE_FLOAT64,
+  TYPE_BOOLEAN, // BOOLEAN EXTENSION
+
+  // ARITHMETIC OPERATORS
+  TYPE_PLUS, // 9
+  TYPE_MINUS,
+  TYPE_MULTIPLY,
+  TYPE_DIVIDE,
+  TYPE_MODULO,
+  //TYPE_INCREMENT, NOT SURE IF WILL BE USED
+  //TYPE_DECREMENT
+
+  // RELATIONAL OPERATORS
+  TYPE_EQUALS, // 14 // not sure if [NOT] EQUALS should be used
+  TYPE_NOT_EQUALS,
+  TYPE_GREATER,
+  TYPE_LESSER,
+  //TYPE_GREATER_OR_EQUAL, NOT SURE IF WILL BE USED
+  //TYPE_LESSER_OR_EQUAL,
+
+  // LOGICAL OPERATORS
+  TYPE_AND, // 18
+  TYPE_OR,
+  TYPE_NOT,
+
+  // ASSIGNMENT OPERATORS
+  TYPE_ASSIGN, // 21
+  TYPE_DECLARATIVE_ASSIGN,
+  TYPE_PLUS_ASSIGN,
+  TYPE_MINUS_ASSIGN,
+  TYPE_MULTIPLY_ASSIGN,
+  TYPE_DIVIDE_ASSIGN,
+  TYPE_MODULO_ASSIGN,
+
+  // MISCELLANEOUS OPERATORS
+  TYPE_ADDRESS, // 28
+  TYPE_POINTER,
+
+  // BRACKET TYPES
+  TYPE_LEFT_BRACKET, // 30
+  TYPE_RIGHT_BRACKET,
+  TYPE_LEFT_SQUARE_BRACKET,
+  TYPE_RIGHT_SQUARE_BRACKET,
+  TYPE_LEFT_CURLY_BRACKET,
+  TYPE_RIGHT_CURLY_BRACKET,
+
+  TYPE_COMMA,
+  TYPE_SEMICOLON,
+
+} TokenType;
+
+/**
+ * @union Various attributes that could be saved for @struct Token. Only one
+ *          is active at a time.
+ */
+typedef union
+{
+  string string;
+  int64_t integer;
+  double float64;
+
+  Keyword keyword;
+} TokenAttribute;
+
+/**
+ * @struct Represents lexical token which holds @enum TokenType as type of
+ *          token and assigned @union TokenAttribute as attribute of token.
+ */
+typedef struct
+{
+  TokenType type;
+  TokenAttribute attribute;
+} Token;
+
+/**
+ * @brief Sets @c file to @var sourceFile.
+ *
+ * @param file
+ * @return 0 if successful, otherwise 1
+ */
+int setSourceFile (FILE *file);
+
+/**
+ * @brief Frees @var sourceFile
+ */
+void unsetSourceFile ();
+
+/**
+ * @brief Checks whether passed string equals to any reserved keyword and if so
+ *          it returns corresponding code to keyword
+ *
+ * @param str - string to compare
+ * @return 0 if no match found
+ *         1-17 depending on match
+ */
+int isReservedKeyword (string *str);
+
+/**
+ * @brief Sets type of @c token to corresponding keyword type by @c keywordType
+ *
+ * @param token
+ * @param keywordType
+ */
+void setTokenKeyword (Token *token, int keywordType);
+
+/**
+ * @brief Reads char after char from @var sourceFile
+ *
+ * @param token
+ * @return 0 if successfully returned token,
+ *         1 if lexical error,
+ *         2 if token equals NULL,
+ *         3 if internal problem
+ */
+int getToken (Token *token);
+#endif //IFJ_PROJ_SCANNER_H
