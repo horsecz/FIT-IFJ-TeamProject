14a2151f1175d36cfc407a5be6752d4ccaa000d9
horsecz <61806439+horsecz@users.noreply.github.com>
2020-11-12 18:16:32 +0100

    Fixed tons of errors, tested, working version now
    It still it may need some deeper tests, but most of the main / edge cases were tested and no problems were found. Also because of development purposes, I decided to change st_id type from string (type from str.h) to char* (things really didn't go well with string type and it wasn't user-friendly nor development-friendly as I expected).

--- symtable.c
+++ symtable.c
@@ -9,14 +9,19 @@
 
 #include "symtable.h"
 
-void destruct_leftmost ( BTNode_t* ptr, tStackP* stack) {
-    while (ptr != NULL) {
-        SPushP(stack, ptr);
-        ptr = ptr->LPtr;
+
+void st_construct ( BTNode_t** symtable ) {
+    *symtable = NULL;
+}
+
+void destruct_leftmost ( BTNode_t** ptr, tStackP* stack) {
+    while (*ptr != NULL) {
+        SPushP(stack, *ptr);
+        *ptr = (*ptr)->LPtr;
     }
 }
 
-void st_destruct ( BTNode_t* symtable ) {
+void st_destruct ( BTNode_t** symtable ) {
     tStackP* stack = (tStackP*) calloc(sizeof(tStackP), 1);
     if (stack == NULL) {
         fprintf(stderr, "Cannot destruct symtable due to internal error. [malloc-stack]\n");
@@ -24,67 +29,72 @@ void st_destruct ( BTNode_t* symtable ) {
     }
 
     SInitP(stack);
-    destruct_leftmost(symtable, stack);
+    destruct_leftmost(&(*symtable), stack);
 
     while (!SEmptyP(stack)) {
-        symtable = STopPopP(stack);
-        if (symtable->RPtr != NULL)
-            destruct_leftmost(symtable->RPtr, stack);
+        *symtable = STopPopP(stack);
+        if ((*symtable)->RPtr != NULL)
+            destruct_leftmost(&(*symtable)->RPtr, stack);
 
-        free(symtable);
-        symtable = NULL;
+        free(*symtable);
+        *symtable = NULL;
     }
 
     free(stack);
 }
 
 int sortStrings ( st_id fst, st_id snd ) {
-    int fst_s = strGetLength(&fst);
-    int snd_s = strGetLength(&snd);
-    int cmp = strCmpString(&fst, &snd);
+    int fst_s = strlen(fst);
+    int snd_s = strlen(snd);
+    int cmp = strcmp(fst, snd);
 
     if (!cmp)
         return 0;
 
     for(int i = 0; i < fst_s && i < snd_s; i++) {
-        if ((&fst)->str[i] > (&snd)->str[i]) {
+        if (fst[i] > snd[i]) {
             return 1;
         }
-        else if ((&fst)->str[i] < (&snd)->str[i]) {
+        else if (fst[i] < snd[i]) {
             return -1;
         }
     }
 
-    if (fst_s > snd_s)
+    if (fst_s > snd_s) {
         return 1;
-    else if (fst_s < snd_s)
+    }
+    else if (fst_s < snd_s) {
         return -1;
+    }
+
+    return 0;
 }
 
-int st_insert ( BTNode_t* symtable, st_id identificator, st_type datatype ) {
+int st_insert ( BTNode_t** symtable, st_id identificator, st_type datatype ) {
     BTNode_t* new = (BTNode_t*) calloc(sizeof(BTNode_t), 1);
     if (new == NULL) { return ST_ERROR; }
 
     new->RPtr = NULL;
     new->LPtr = NULL;
-    new->id = identificator;
     new->type = datatype;
+    new->id = identificator;
 
-    if (symtable == NULL) {
-        symtable = new;
+
+    if (*symtable == NULL) {
+        *symtable = new;
         return ST_SUCCESS;
     }
 
-    if (!strCmpString(&symtable->id, &identificator)) {
+    if (!strcmp((*symtable)->id, new->id)) {
         free(new);
         return ST_ID_EXISTS;
     }
 
-    BTNode_t* temp = symtable;
+    BTNode_t* temp = *symtable;
     int st_sort = 0;
 
     while (temp->LPtr != NULL || temp->RPtr != NULL) {
-        st_sort = sortStrings(temp->id, identificator);
+        st_sort = sortStrings(temp->id, new->id);
         if (temp->LPtr != NULL && (st_sort > 0))
             temp = temp->LPtr;
         else if (temp->RPtr != NULL && (st_sort < 0 ))
@@ -97,7 +107,7 @@ int st_insert ( BTNode_t* symtable, st_id identificator, st_type datatype ) {
             break;
     }
 
-    st_sort = sortStrings(temp->id, identificator);
+    st_sort = sortStrings(temp->id, new->id);
     if (st_sort > 0)
         temp->LPtr = new;
     else if (st_sort < 0)
@@ -110,15 +120,15 @@ int st_insert ( BTNode_t* symtable, st_id identificator, st_type datatype ) {
     return ST_SUCCESS;
 }
 
-BTNode_t* st_lookup ( BTNode_t* symtable, st_id identificator ) {
-    BTNode_t* temp = symtable;
+BTNode_t* st_lookup ( BTNode_t** symtable, st_id identificator ) {
+    BTNode_t* temp = *symtable;
     int st_sort = 0;
 
     while (temp != NULL) {
         st_sort = sortStrings(temp->id, identificator);
-        if (st_sort) //left
+        if (st_sort > 0) //left
             temp = temp->LPtr;
-        else if (!st_sort) //right
+        else if (st_sort < 0) //right
             temp = temp->RPtr;
         else
             break;
@@ -127,56 +137,100 @@ BTNode_t* st_lookup ( BTNode_t* symtable, st_id identificator ) {
     return temp;
 }
 
-void delete_ReplaceByRightmost (BTNode_t* PtrReplaced, BTNode_t* RootPtr) {
-    while (RootPtr != NULL && RootPtr->RPtr != NULL)
-        RootPtr = RootPtr->RPtr;
+void delete_ReplaceByRightmost (BTNode_t* PtrReplaced, BTNode_t** RootPtr) {
+    while ((*RootPtr) != NULL && (*RootPtr)->RPtr != NULL)
+        *RootPtr = (*RootPtr)->RPtr;
 
     // expecting RootPtr != NULL
-    BTNode_t* work = RootPtr;
-    PtrReplaced->type = RootPtr->type;
-    PtrReplaced->id = RootPtr->id;
+    BTNode_t* work = *RootPtr;
+    PtrReplaced->type = (*RootPtr)->type;
+    PtrReplaced->id = (*RootPtr)->id;
     free(work);
     work = NULL;
     return;
 }
 
-int st_delete ( BTNode_t* symtable, st_id identificator ) {
-    BTNode_t* removal = st_lookup(symtable, identificator);
+int st_delete ( BTNode_t** symtable, st_id identificator ) {
+    BTNode_t* removal = *symtable;
+    BTNode_t* removal_p = *symtable;
+    int st_sort = 0;
+
+
+    while (removal != NULL) {
+        st_sort = sortStrings((removal)->id, identificator);
+        if (st_sort == 0)
+            break;
+
+        removal_p = removal;
+        if (st_sort > 0) //left
+            removal = (removal)->LPtr;
+        else if (st_sort < 0) //right
+            removal = (removal)->RPtr;
+    }
+
     if (removal == NULL) return ST_ERROR;
 
-    if (removal->LPtr == NULL && removal->RPtr == NULL) {
+    if ((removal)->LPtr == NULL && (removal)->RPtr == NULL) {
+        if (removal_p != removal) {
+            if (removal_p->RPtr == removal)
+                removal_p->RPtr = NULL;
+            else
+                removal_p->LPtr = NULL;
+
+        } else {
+            (*symtable) = NULL;
+        }
+
         free(removal);
         removal = NULL;
-    } else if (removal->LPtr != NULL && removal->RPtr != NULL) {
+    } else if ((removal)->LPtr != NULL && (removal)->RPtr != NULL) {
         BTNode_t* LPtr = removal->LPtr->LPtr;
-        delete_ReplaceByRightmost(removal, removal->LPtr);
-        if (removal->LPtr == NULL)
-            removal->LPtr = LPtr;
-
+        delete_ReplaceByRightmost((removal), &(removal)->LPtr);
+        (removal)->LPtr = LPtr;
     } else {
-        BTNode_t* workLPtr = removal->LPtr;
-        BTNode_t* workRPtr = removal->RPtr;
+        BTNode_t* workLPtr = (removal)->LPtr;
+        BTNode_t* workRPtr = (removal)->RPtr;
         BTNode_t* newRoot = NULL;
 
-        if (removal->LPtr != NULL) {
+        if ((removal)->LPtr != NULL) {
             newRoot = workLPtr;
         } else {
             newRoot = workRPtr;
         }
 
-        free(removal);
-        removal = newRoot;
+        if (removal_p->LPtr == removal)
+            removal_p->LPtr = newRoot;
+        else if (removal_p->RPtr == removal)
+            removal_p->RPtr = newRoot;
+
+        if (removal != (*symtable)) {
+            free(removal);
+            removal = NULL;
+        } else {
+            if (removal->LPtr != NULL) {
+                removal_p = removal->LPtr;
+            }
+            else if (removal->RPtr != NULL) {
+                removal_p = removal->RPtr;
+            }
+            else {
+                removal_p = NULL;
+            }
+
+            (*symtable) = removal_p;
+            free(removal);
+        }
     }
 
     return ST_SUCCESS;
 }
 
-st_type st_get_type ( BTNode_t* id_node ) {
-    return id_node->type;
+st_type st_get_type ( BTNode_t** id_node ) {
+    return (*id_node)->type;
 }
 
-void st_set_type ( BTNode_t* id_node, st_type datatype ) {
-    id_node->type = datatype;
+void st_set_type ( BTNode_t** id_node, st_type datatype ) {
+    (*id_node)->type = datatype;
     return;
 }
 
