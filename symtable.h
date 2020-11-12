f2261df2405b7a93e39632c1e6a2080bc061416e
horsecz <61806439+horsecz@users.noreply.github.com>
2020-11-12 18:16:32 +0100

    Fixed tons of errors, tested, working version now
    It still it may need some deeper tests, but most of the main / edge cases were tested and no problems were found. Also because of development purposes, I decided to change st_id type from string (type from str.h) to char* (things really didn't go well with string type and it wasn't user-friendly nor development-friendly as I expected).

--- symtable.h
+++ symtable.h
@@ -13,20 +13,23 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <stdbool.h>
-#include "str.h"
+#include <string.h>
 
 /*
  * USER FUNCTIONS, TYPES, ENUMS, ..
  */
 
-#define ST_SUCCESS 0
-#define ST_ERROR 1
+#define ST_SUCCESS 1
+#define ST_ERROR 0
 #define ST_ID_EXISTS -1
 #define ST_MAXSTACK CHAR_MAX
 
-typedef string st_id;
+typedef char* st_id;
 
-typedef enum {
+typedef enum st_datatypes {
+    // INTERNAL
+    UNKNOWN,
+
     // BASIC DATA TYPES
     INT,
     STRING,
@@ -47,30 +50,29 @@ typedef struct {
     int top;
 } tStackP;
 
-#define st_construct(symtable) { \
-            BTNode_t* symtable = NULL;  \
-}
-// Symtable must be new non-existing BTNode_t* (pointer to struct BTNode) named 'symtable'
-// Otherwise (GCC) compiler prints error message (redefinition, redeclaration of 'symtable')
+/*
+void st_construct ( BTNode_t* symtable );*/
+void st_construct ( BTNode_t** symtable );
+// Sets symtable to NULL
 
-void st_destruct ( BTNode_t* symtable );
+void st_destruct ( BTNode_t** symtable );
 // Destroys symtable
 
-int st_insert ( BTNode_t* symtable, st_id identificator, st_type datatype );
+int st_insert ( BTNode_t** symtable, st_id identificator, st_type datatype );
 // Inserts new node with key 'identificator' and data 'datatype' in symtable 'symtable'.
 // Returns >0 if ok, 0 if insert failed (malloc, ..), <0 if 'identificator' already exists in 'symtable'
 
-int st_delete ( BTNode_t* symtable, st_id identificator );
+int st_delete ( BTNode_t** symtable, st_id identificator );
 // Returns >0 if ok, 0 if remove failed, because 'identificator' isnt present in 'symtable'
 
-BTNode_t* st_lookup ( BTNode_t* symtable, st_id identificator );
+BTNode_t* st_lookup ( BTNode_t** symtable, st_id identificator );
 // Looks up for 'identificator' in 'symtable'
-// Returns pointer to node containing 'identificator' key or NULL if not found.
+// Returns pointer to (pointer to) node containing 'identificator' key or NULL if not found.
 
-st_type st_get_type ( BTNode_t* id_node );
+st_type st_get_type ( BTNode_t** id_node );
 // Returns type of 'id_node' identificator, which is expected to be != NULL (or expect undefined behaviour)
 
-void st_set_type ( BTNode_t* id_node, st_type datatype );
+void st_set_type ( BTNode_t** id_node, st_type datatype );
 // Sets type of 'id_node" identificator to 'datatype'
 // Notes: id_node should be NULL and datatype should be from D_TYPE enum (for both cases: expect undefined behaviour)
 
