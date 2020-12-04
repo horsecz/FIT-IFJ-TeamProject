01520f4a7599e668aa1104614b8d5073b0f45796
zelick0 <57447396+zelick0@users.noreply.github.com>
2020-12-04 20:38:34 +0100

    Add files via upload

---
+++ precedent.h
@@ -0,0 +1,40 @@
+
+
+#ifndef _PRECEDENT_H
+#define _PRECEDENT_H
+
+#define SCANNER_OK      0
+#define SYNTAX_OK       0
+#define SCANNER_ERROR   9   
+#define SYNTAX_ERROR    9
+#define SEM_ERROR_DEF 	9
+#define SEM_ERROR_TYPE  9
+#define SEM_ERROR_PARAM 9
+#define SEM_ERROR_OTHER 9
+#define ZERO_DIVISION   9
+#define INTERNAL_ERROR  99
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "scanner.h"
+#include "symtable.h"
+#include "symstack.h"
+#include "error.h"
+#include "string.h"
+
+int precedent_analys(Token* help, Token* tokeng, TokenType* Type);
+
+int idkfunkce(symStack *stack, Token* token, TokenType* Type);
+
+TokenType StackTopTerm (symStack *stack);
+
+int stackPushOpen(symStack *stack); 
+
+symStackItem* stackPosition(symStack *stack, int j);
+
+int reduction(symStack *stack);
+
+void setInFunction(bool in);
+
+#endif
